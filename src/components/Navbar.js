39c4acd40d3ea9097476c1dd9e257dc2a70147b6
Jamsheed-Khan <jamshedkh365@gmail.com>
2024-02-26 00:32:40 +0500

    almost done.....

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-          <h1 style={{fontSize:"150%" , color:"#c770f0", fontWeight:"bolder"}}>Jamsheed Khan.</h1>
+          <h1 style={{fontSize:"150%" , color:"#c770f0", fontWeight:"bolder"}}>Jamsheed.</h1>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
